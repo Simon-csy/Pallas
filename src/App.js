976647c777744cea0ec9c70d6af8ae13341f04d7
Simon-csy <ceres.1.sc@gmail.com>
2024-08-09 13:58:44 +0900

    update timeline, add link and google books page

--- src/App.js
+++ src/App.js
@@ -7,31 +7,37 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineDot from '@mui/lab/TimelineDot';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
+import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import TimelineOppositeContent, {
+  timelineOppositeContentClasses,
+} from '@mui/lab/TimelineOppositeContent';
 
-// const TimelineItemContainer = styled('div')(({ theme }) => ({
-//   display: 'flex',
-//   alignItems: 'center',
+// const LeftContent = styled('div')(({ theme }) => ({
+//   flex: 1,
+//   textAlign: 'right',
+//   paddingRight: theme.spacing(2),
+//   marginTop: theme.spacing(1),
 // }));
-
-const LeftContent = styled('div')(({ theme }) => ({
-  flex: 1,
-  textAlign: 'right',
-  paddingRight: theme.spacing(2),
-  marginTop: theme.spacing(1), // Add margin to the top
-}));
 
 const RightContent = styled('div')(({ theme }) => ({
   flex: 1,
   textAlign: 'left',
   paddingLeft: theme.spacing(2),
-  marginTop: theme.spacing(0.5), // Add margin to the top
+  marginTop: theme.spacing(0.5),
 }));
 
-export default function BasicTimeline() {
+const BookContainer = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  flexWrap: 'nowrap',
+});
+
+export default function LeftAlignedTimeline() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    // Replace 'your-api-endpoint' with the actual API endpoint
     axios.get('https://www.simonland.me/api/book')
       .then(response => {
         console.log(response.data);
@@ -51,28 +57,56 @@ export default function BasicTimeline() {
     return `${year}/${month}/${day}`;
   };
 
-  return (
-    <Timeline>
-      {events.map((event, index) => {
-        const startDate = formatDate(event.start);
-        const endDate = formatDate(event.end);
-        const displayDate = startDate === endDate ? startDate : `${startDate} ~ ${endDate}`;
+    return (
+      <Timeline sx={{
+        [`& .${timelineOppositeContentClasses.root}`]: {
+          flex: 0.2,
+        },
+      }}>
+        {events.map((event, index) => {
+          const startDate = formatDate(event.start);
+          const endDate = formatDate(event.end);
+          const displayDate = startDate === endDate ? startDate : `${startDate} ~ ${endDate}`;
 
-        return (
-          <TimelineItem key={index}>
-            <LeftContent>
-              <Typography variant="body1">{displayDate}</Typography>
-            </LeftContent>
-            <TimelineSeparator>
-              <TimelineDot />
-              {index < events.length - 1 && <TimelineConnector />}
-            </TimelineSeparator>
-            <RightContent>
-              <Typography variant="h6">{event.book.name}</Typography>
-            </RightContent>
-          </TimelineItem>
-        );
-      })}
-    </Timeline>
-  );
-}
+          return (
+            <TimelineItem key={index}>
+              <TimelineOppositeContent >
+                <Typography variant="body1">{displayDate}</Typography>
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineDot />
+                {index < events.length - 1 && <TimelineConnector />}
+              </TimelineSeparator>
+              <RightContent>
+                <BookContainer>
+                  {event.book.link ? (
+                    <Link
+                      href={`https://www.simonland.me/${event.book.link}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ display: 'inline-flex', alignItems: 'center' }}
+                    >
+                      <Typography variant="h6" style={{ marginRight: '8px' }}>{event.book.name}</Typography>
+                    </Link>
+                  ) : (
+                    <Typography variant="h6" style={{ marginRight: '8px' }}>{event.book.name}</Typography>
+                  )}
+                  {event.book.google && event.book.google.split(',').map((url, index) => (
+                    <IconButton
+                      key={index}
+                      size="small"
+                      href={url.trim()}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <OpenInNewIcon fontSize="small" />
+                    </IconButton>
+                  ))}
+                </BookContainer>
+              </RightContent>
+            </TimelineItem>
+          );
+        })}
+      </Timeline>
+    );
+  }
